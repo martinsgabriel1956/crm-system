8771bdeaf16ec6da8301bffbef1c19ca39668f4f
Gabriel Martins <martinsgabriel1956@gmail.com>
2024-03-07 15:07:15 -0300

    chore: set the server and .env file

---
+++ api/src/server.ts
@@ -0,0 +1,10 @@
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server started on port ${process.env.PORT}`);
+});
